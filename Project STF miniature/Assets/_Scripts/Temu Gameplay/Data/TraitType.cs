a413c9e084787d2980b8169903acc6565164394d
Blaer Zhang <zhangyueyang0022@126.com>
2025-06-23 12:09:25 -0400

    Poker版简易ItemDataSO

--- Project STF miniature/Assets/_Scripts/Temu Gameplay/Data/TraitType.cs
+++ Project STF miniature/Assets/_Scripts/Temu Gameplay/Data/TraitType.cs
@@ -18,5 +18,12 @@ namespace TemuGameplay.Data
         Medic,
         Booster,
         Movement,
+        
+        Straight,
+        Flush,
+        Kind,
+        FourSuits,
+        Pairs,
+        Faces,
     }
 } 
